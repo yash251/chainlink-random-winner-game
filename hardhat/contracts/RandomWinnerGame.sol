eb52a10b453c86422d67bc38d8ede4df0ab3c5ab
Yash Jagtap <jagtapyash2512@gmail.com>
2023-07-31 23:44:46 +0530

    RandomWinnerGame blpt

---
+++ hardhat/contracts/RandomWinnerGame.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/access/Ownable";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+
+contract RandomWinnerGame is VRFConsumerBase, Ownable {
+    
+}

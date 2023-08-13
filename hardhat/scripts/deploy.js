181317ca3d9082193e8c0d2d3cc100d37ecca38d
Yash Jagtap <jagtapyash2512@gmail.com>
2023-08-13 11:38:50 +0530

    deploy script

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,33 +1,42 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { FEE, VRF_COORDINATOR, LINK_TOKEN, KEY_HASH } = require("../constants");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  /*
+ DeployContract in ethers.js is an abstraction used to deploy new smart contracts,
+ so randomWinnerGame here is a factory for instances of our RandomWinnerGame contract.
+ */
+   // deploy the contract
+   const randomWinnerGame = await hre.ethers.deployContract(
+     "RandomWinnerGame",
+     [VRF_COORDINATOR, LINK_TOKEN, KEY_HASH, FEE]
+   );
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  await randomWinnerGame.waitForDeployment();
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
+   // print the address of the deployed contract
+   console.log("Verify Contract Address:", randomWinnerGame.target);
+
+  console.log("Sleeping.....");
+  // Wait for etherscan to notice that the contract has been deployed
+  await sleep(30000);
+
+  // Verify the contract after deploying
+  await hre.run("verify:verify", {
+    address: randomWinnerGame.target,
+    constructorArguments: [VRF_COORDINATOR, LINK_TOKEN, KEY_HASH, FEE],
   });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
